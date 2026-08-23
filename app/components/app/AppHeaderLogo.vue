f017809d67a686b0f2a13027fd6d58062712c9ed
Alwyn974 <alwyn974@gmail.com>
2026-08-24 00:26:51 +0400

    fix logo rendering and add build CI

--- app/components/app/AppHeaderLogo.vue
+++ app/components/app/AppHeaderLogo.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { withBase } from 'ufo'
+
 const appConfig = useAppConfig()
 const { hasLogo, headerLightUrl, headerDarkUrl, contextMenuItems } = useLogoAssets()
+const baseURL = useRuntimeConfig().app.baseURL
+const lightSrc = computed(() => withBase(headerLightUrl.value, baseURL))
+const darkSrc = computed(() => withBase(headerDarkUrl.value, baseURL))
 </script>
 
 <template>
@@ -8,12 +13,15 @@ const { hasLogo, headerLightUrl, headerDarkUrl, contextMenuItems } = useLogoAsse
     v-if="hasLogo"
     :items="contextMenuItems"
   >
-    <UColorModeImage
-      provider="none"
-      :light="headerLightUrl"
-      :dark="headerDarkUrl"
+    <img
+      :src="lightSrc"
       :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
-      :class="['h-6 w-auto shrink-0', appConfig.header?.logo?.class]"
+      :class="['h-6 w-auto shrink-0 dark:hidden', appConfig.header?.logo?.class]"
+    >
+    <img
+      :src="darkSrc"
+      :alt="appConfig.header?.logo?.alt || appConfig.header?.title"
+      :class="['hidden h-6 w-auto shrink-0 dark:block', appConfig.header?.logo?.class]"
     />
   </UContextMenu>
   <span v-else>
